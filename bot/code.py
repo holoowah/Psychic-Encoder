a241001e50a16a3fa4b30f133c9dc663708ae585
Dark-super-me <87744886+Dark-super-me@users.noreply.github.com>
2021-10-19 19:57:07 +0530

    Create code.py

---
+++ bot/code.py
@@ -0,0 +1,5 @@
+# the following content contains ffmpeg violence 
+# kindly only 15+ learn it, else u will not study and open a channel like (c) @Animes_Encoded and waste ur time 
+# kindly teens only !!
+# (c) No Abhir Hassan allowed ....
+
